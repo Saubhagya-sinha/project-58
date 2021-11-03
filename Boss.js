9ab4364a1b28274bef16709a624e80a5cefaf195
Saubhagya-sinha <83703356+Saubhagya-sinha@users.noreply.github.com>
2021-11-03 17:03:04 +0530

    Add files via upload

---
+++ Boss.js
@@ -0,0 +1,9 @@
+function one() {
+    window.alert("Mahendra Singh Dhoni is a former Indian international cricketer who captained the Indian ... Nickname, Mahi, Captain Cool, MSD, Thala. Height, 1.80 m (5 ft 11 in). Batting, Right-handed. Bowling, Right-arm medium. Role, Wicket-keeper batsman..");
+}
+
+function two() {
+    window.alert("is an Indian former international cricketer who served as captain of the Indian national team. He is widely regarded as one of the greatest batsmen in the history of cricket. He is the highest run scorer of all time in international cricket, and the only player to have scored one hundred international centuries, the first batsman to score a double century in a One Day International (ODI), the holder of the record for the most runs in both Test and ODI cricket, and the only player to complete more than 30,000 runs in international cricket.[7] In 2013, he was the only Indian cricketer included in an all-time Test World XI named to mark the 150th anniversary of Wisden Cricketers Almanack.He is affectionately known as Little Master or Master Blaster.
+
+        "Tendulkar took up cricket at the age of eleven, made his Test debut on 15 November 1989 against Pakistan in Karachi at the age of sixteen, and went on to represent Mumbai domestically and India internationally for close to twenty-four years. In 2002, halfway through his career, Wisden Cricketers Almanack ranked him the second-greatest Test batsman of all time, behind Don Bradman, and the second-greatest ODI batsman of all time, behind Viv Richards. Later in his career, Tendulkar was a part of the Indian team that won the 2011 World Cup, his first win in six World Cup appearances for India. He had previously been named Player of the Tournament at the 2003 edition of the tournament held in South Africa")
+}
